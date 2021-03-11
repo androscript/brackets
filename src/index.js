116c3247d69f0e8a17228d68578676d3ff096ea5
androscript <androscript@gmail.com>
2021-03-11 18:11:34 +0200

    feat: add solution for Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  } else {
+      for (var i = 0; i < bracketsConfig.length; i++) {
+        if (str.includes(bracketsConfig[i].join(''))) {
+          str = str.replace(bracketsConfig[i].join(''), '');
+          if (str.length === 0) {
+            return true;
+          } else {
+              i = -1;
+          };
+        };
+      };
+    return false;
+  };
 }
